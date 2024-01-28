d944494eff8d89732ccad6e4b421047bd0f8afb1
ImSahilShaikh <imsas.shaikh3@gmail.com>
2024-01-29 01:02:54 +0530

    Controller skeleton updated

--- src/main/java/com/url/shortner/controller/ShortenController.java
+++ src/main/java/com/url/shortner/controller/ShortenController.java
@@ -1,12 +1,22 @@
 package com.url.shortner.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ShortenController {
-    @GetMapping("/hello")
-    public String helloController(){
-        return "Sahil";
+    @GetMapping("/getShortenedUrl")
+    public String getShortenedUrl(){
+        //TODO - to add URL fetch logic from a db (nosql is planned to be used)
+        String url = "";
+        //TODO - must return Http entity to handle error codes
+        return url;
+    }
+
+    @PostMapping("/createShortUrl")
+    public void createShortUrl(@RequestBody String urlInfo){
+        //TODO - must return Http entity to handle error codes
     }
 }
