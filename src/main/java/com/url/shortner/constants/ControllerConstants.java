58a84b34af34eda2d6f0066197e6c6ef9d0cf705
ImSahilShaikh <imsas.shaikh3@gmail.com>
2024-03-09 15:47:04 +0530

    constants added

--- src/main/java/com/url/shortner/constants/ControllerConstants.java
+++ src/main/java/com/url/shortner/constants/ControllerConstants.java
@@ -1,4 +1,7 @@
 package com.url.shortner.constants;
 
 public class ControllerConstants {
+    public static final String GET_ALL_URLS = "/getAll";
+    public static final String CREATE_SHORT_URL = "/createShortUrl";
+    public static final String REDIRECT_TO_ORIGINAL_URL = "/{shortLink}";
 }
--- src/main/java/com/url/shortner/controller/ShortenController.java
+++ src/main/java/com/url/shortner/controller/ShortenController.java
@@ -1,13 +1,18 @@
 package com.url.shortner.controller;
 
 import com.url.shortner.ShortenerUtilities;
+import com.url.shortner.constants.ControllerConstants;
 import com.url.shortner.model.Url;
 import com.url.shortner.repo.UrlRepository;
 import com.url.shortner.service.UrlShorteningService;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
 import java.time.LocalDateTime;
@@ -21,12 +26,12 @@ public class ShortenController {
     @Autowired
     UrlShorteningService shorteningService;
 
-    @GetMapping("/getShortenedUrl")
+    @GetMapping(ControllerConstants.GET_ALL_URLS)
     public ResponseEntity<?> getShortenedUrl(){
         return ResponseEntity.ok(this.urlRepository.findAll());
     }
 
-    @PostMapping("/createShortUrl")
+    @PostMapping(ControllerConstants.CREATE_SHORT_URL)
     public ResponseEntity<?> createShortUrl(@RequestBody Url urlInfo){
         System.out.println(shorteningService.generateShortUrl(urlInfo));
         urlInfo.setShortenedUrl(ShortenerUtilities.getShortenedUrl(urlInfo.getUrl()));
@@ -37,7 +42,7 @@ public class ShortenController {
         return ResponseEntity.ok(url);
     }
 
-    @GetMapping("/{shortLink}")
+    @GetMapping(ControllerConstants.REDIRECT_TO_ORIGINAL_URL)
     public ResponseEntity<?> redirectToOriginalUrl(@PathVariable String shortLink, HttpServletResponse response) throws IOException {
         Url urlToRedirect = this.urlRepository.findByShortenedUrl(shortLink);
         response.sendRedirect(urlToRedirect.getUrl());
